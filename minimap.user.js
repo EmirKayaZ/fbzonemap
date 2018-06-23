6104c0057dc94b3e9b991b061d2bd3a69508f93e
fenerzone <40503804+fenerzone@users.noreply.github.com>
2018-06-24 02:17:06 +0300

    Update minimap.user.js

--- minimap.user.js
+++ minimap.user.js
@@ -12,7 +12,7 @@
 // @grant        none
 // ==/UserScript==
 
-Number.prototype.between = function(a, b) {
+ Number.prototype.between = function(a, b) {
   var min = Math.min.apply(Math, [a, b]),
     max = Math.max.apply(Math, [a, b]);
   return this > min && this < max;
